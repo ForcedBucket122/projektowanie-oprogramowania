2bab5a3a0612678dd4fd5a919a0250cf458758bb
ForcedBucket122 <dzejkob2112@gmail.com>
2024-09-19 14:10:38 +0200

    cos

---
+++ sortowanie/sort babelkowe/babelkowe/babelkowe/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class babelkowe
+{
+    static void Main()
+    {
+        
+        int[] array= new int[6];
+        for(int i = 0; i < 6; i++)
+        {
+            Console.Write("Podaj liczbe: ");
+            int liczba = int.Parse(Console.ReadLine());
+            array[i] = liczba;
+        }
+        BubbleSort(array);
+
+        Console.WriteLine("Posortowana tablica:");
+        foreach (int item in array)
+        {
+            Console.Write(item + " ");
+        }
+    }
+
+    static void BubbleSort(int[] array)
+    {
+        int n = array.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    // Zamiana miejscami
+                    Console.WriteLine(String.Join(", ", array));
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
